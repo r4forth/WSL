d0a35c0c79e3b733269d180b56122d025638ee4e
r4forth <r4forth@gmail.com>
2019-10-25 07:31:41 +0800

    Update Raspbian2019.sh

--- Raspbian2019.sh
+++ Raspbian2019.sh
@@ -13,6 +13,8 @@ sudo apt-get upgrade -y
 # =============================================================================
 # 安裝必要套件
 sudo apt-get install -y vim git
+# 讀取 ntfs 磁碟
+sudo apt install -y ntfs-3g
 sudo apt autoremove -y
 # 設定 vim 套件管理系統
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -30,11 +32,32 @@ cp ~/myBuster/vimrc ~/.vimrc
 # 安裝基本程式開發工具
 sudo apt-get install -y build-essential
 
+# =============================================================================
+# 安裝教育類軟體
+# =============================================================================
+# 鍵盤按鈕輔助工具
+sudo apt install -y key-mon
 
 # =============================================================================
-# 安裝  環境
+# 安裝中文環境
 # =============================================================================
-
+# 中文輸入法
+sudo apt install -y fcitx fcitx-chewing
+# 常用中文字型
+# 文泉驛微米黑
+sudo apt-get install -y ttf-wqy-microhei
+# 文泉驛正黑
+sudo apt-get install -y ttf-wqy-zenhei
+# 文泉驛點陣宋體 X11
+sudo apt-get install -y xfonts-wqy
+# 教育部標準楷書
+sudo apt-get install -y fonts-moe-standard-kai
+# 教育部標準宋體
+sudo apt-get install -y fonts-moe-standard-song
+# 文鼎楷書體 Unicode
+sudo apt-get install -y fonts-arphic-ukai
+# 文鼎明體 Unicode
+sudo apt-get install -y fonts-arphic-uming
 
 # =============================================================================
 # 安裝 nodejs 環境
